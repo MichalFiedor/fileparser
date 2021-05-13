066320710324164740e04b8833d2261a3905185a
MichalFiedor <michal.fiedor92@gmail.com>
2021-05-13 17:31:52 +0200

    Configuration.

---
+++ src/main/java/com/fiedormichal/RestFileParser/RestFileParserApplication.java
@@ -0,0 +1,13 @@
+package com.fiedormichal.RestFileParser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFileParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestFileParserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiedormichal/RestFileParser/RestFileParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiedormichal.RestFileParser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestFileParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
